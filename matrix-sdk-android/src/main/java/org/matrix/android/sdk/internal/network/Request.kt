119b378f3d077bad85987f3bff437aadacf0340c
Tim van Lieshout <tim@mylermedia.nl>
2023-04-13 12:46:40 +0200

    no longer throws any exception when request fails

--- matrix-sdk-android/src/main/java/org/matrix/android/sdk/internal/network/Request.kt
+++ matrix-sdk-android/src/main/java/org/matrix/android/sdk/internal/network/Request.kt
@@ -92,14 +92,14 @@ internal suspend inline fun <DATA> executeRequest(
                 delay(currentDelay)
                 currentDelay = currentDelay.times(2L).coerceAtMost(maxDelayBeforeRetry)
                 // Try again (loop)
-            } else {
-                throw when (exception) {
-                    is IOException -> Failure.NetworkConnection(exception)
-                    is Failure.ServerError,
-                    is Failure.OtherServerError,
-                    is CancellationException -> exception
-                    else -> Failure.Unknown(exception)
-                }
+//            } else {
+//                throw when (exception) {
+//                    is IOException -> Failure.NetworkConnection(exception)
+//                    is Failure.ServerError,
+//                    is Failure.OtherServerError,
+//                    is CancellationException -> exception
+//                    else -> Failure.Unknown(exception)
+//                }
             }
         }
     }
